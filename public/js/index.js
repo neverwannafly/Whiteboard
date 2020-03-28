eb9a4fbb50847c9cd2c39030a001b7bacbe50d11
Shubham Anand <shubham_chess@live.com>
2020-03-28 23:47:18 +0530

    set whiteboard background to black

--- public/js/index.js
+++ public/js/index.js
@@ -5,10 +5,10 @@
     const resolution = RESOLUTION_HD;
     const height = window.innerHeight * 0.95;
     const width = window.outerWidth * 0.95;
-    const color = COLOR_BLACK;
     
     let whiteboard = new Whiteboard(canvas, width, height, resolution);
-    whiteboard.setColor(color);
+    whiteboard.setColor(COLOR_RED);
+    whiteboard.setBackground(COLOR_BLACK);
     whiteboard.setWidth(2);
     whiteboard.setMode(MODE_DRAW);
 
