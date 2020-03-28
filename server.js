724cbf83d9a1afe978adfb328b272d8f730897a2
Shubham Anand <shubham_chess@live.com>
2020-03-28 08:15:27 +0530

    create server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
