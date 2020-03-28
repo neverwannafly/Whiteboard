9cb006938e72577131a3628dd540ff3dd7b61501
Shubham Anand <shubham_chess@live.com>
2020-03-28 22:36:08 +0530

    change whiteboard setup

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,14 @@
     const canvas = document.getElementById("whiteboard");
 
     const resolution = RESOLUTION_HD;
-    const height = window.innerHeight * 0.8;
-    const width = window.outerWidth * 0.8;
+    const height = window.innerHeight * 0.95;
+    const width = window.outerWidth * 0.95;
     const color = COLOR_BLACK;
     
     let whiteboard = new Whiteboard(canvas, width, height, resolution);
     whiteboard.setColor(color);
-    whiteboard.setWidth(3);
+    whiteboard.setWidth(2);
+    whiteboard.setMode(MODE_DRAW);
 
     whiteboard.startEventLoop();
     
