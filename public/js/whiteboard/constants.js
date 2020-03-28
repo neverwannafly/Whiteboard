78cb27c9a40c1f828ce15b41e5c7323fc8a6e125
Shubham Anand <shubham_chess@live.com>
2020-03-28 22:34:57 +0530

    add modes and states

--- public/js/whiteboard/constants.js
+++ public/js/whiteboard/constants.js
@@ -1,12 +1,21 @@
 // Resolutions
-const RESOLUTION_HD           =       4;
-const RESOLUTION_HIGH         =       2;
-const RESOLUTION_STANDARD     =       1;
-const RESOLUTION_LOW          =     0.5;
+const RESOLUTION_HD                 =       4;
+const RESOLUTION_HIGH               =       2;
+const RESOLUTION_STANDARD           =       1;
+const RESOLUTION_LOW                =     0.5;
 
 // Colors
-const COLOR_RED                   =       "#FF0000";
-const COLOR_BLUE                  =       "#0000FF";
-const COLOR_GREEN                 =       "#00FF00";
-const COLOR_BLACK                 =       "#000000";
-const COLOR_PINK                  =       "#FF00FF";
+const COLOR_RED                     =       "#FF0000";
+const COLOR_BLUE                    =       "#0000FF";
+const COLOR_GREEN                   =       "#00FF00";
+const COLOR_BLACK                   =       "#000000";
+const COLOR_PINK                    =       "#FF00FF";
+
+// Drawing Modes
+const MODE_IDLE                     =       0b0000;  
+const MODE_DRAW                     =       0b0001;
+const MODE_ERASE                    =       0b0010;
+
+// Whiteboard States
+const STATE_PEN_UP                  =       false;
+const STATE_PEN_DOWN                =       true;
